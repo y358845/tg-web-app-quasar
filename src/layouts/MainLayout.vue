58707563f0c6cd49876314bb2662d1c502cb60f5
irlandets1980 <irlandets1980@gmail.com>
2023-05-01 15:09:54 +0300

    first commit347

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -114,7 +114,7 @@ export default defineComponent({
         //   date: this.date,
         //   date2: this.date2
         // }
-        tg.sendData(JSON.stringify(this.surname));
+        tg.sendDatathis(surname);
         // console.log(dataForm)
         // console.log(dataForm)
       }
