6d99d493c8069e66b74029f1941b911e3fb79d40
irlandets1980 <irlandets1980@gmail.com>
2023-05-04 23:19:45 +0300

    fmy70

--- src/pages/ErrorNotFound.vue
+++ src/pages/ErrorNotFound.vue
@@ -2,22 +2,14 @@
   <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
     <div>
       <div style="font-size: 30vh">
-        404
+        777
       </div>
 
       <div class="text-h2" style="opacity:.4">
-        Oops. Nothing here...
+        Попался!!!
       </div>
 
-      <q-btn
-        class="q-mt-xl"
-        color="white"
-        text-color="blue"
-        unelevated
-        to="/"
-        label="Go Home"
-        no-caps
-      />
+      <q-btn class="q-mt-xl" color="white" text-color="blue" unelevated to="/" label="Go Home" no-caps />
     </div>
   </div>
 </template>
