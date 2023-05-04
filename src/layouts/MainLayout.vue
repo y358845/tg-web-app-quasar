b16bf8554ad48e2072a4af2fb86133e6dadeea44
irlandets1980 <irlandets1980@gmail.com>
2023-05-04 22:13:34 +0300

    fmy57

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -169,7 +169,7 @@ export default defineComponent({
         }
       },
       mainButtonClicked() {
-        tg.showAlert('Что за хроень');
+        tg.showAlert(this.data);
         // window.Telegram.WebApp.showAlert('Main button was pressed version2');
         // const dataForm = {
         //   surname: this.surname,
