cd148bf40db1ff6fc47a4126b1a03864caa6f3d2
irlandets1980 <irlandets1980@gmail.com>
2023-07-04 17:49:21 +0300

    1izm11

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,13 +1,13 @@
 const routes = [{
     path: '/',
-    component: () => import('pages/PassOneDate.vue'),
+    component: () => import('pages/PassTwoDate.vue'),
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/PassOneDate.vue')
+    component: () => import('pages/PassTwoDate.vue')
   }
 ]
 
