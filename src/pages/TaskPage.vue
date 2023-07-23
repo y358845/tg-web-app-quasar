f17cc6dbc6d92ed8289fe891532923a90a3c1eb2
irlandets1980 <irlandets1980@gmail.com>
2023-07-23 10:54:52 +0300

    client0045

--- src/pages/TaskPage.vue
+++ src/pages/TaskPage.vue
@@ -104,7 +104,7 @@ export default defineComponent({
       this.sendInquiry()
     },
     startParam() {
-      this.test = JSON.stringify(window?.Telegram?.WebView)
+      this.test = JSON.stringify(window?.Telegram?.WebView.initParams)
       // this.test = window.Telegram.WebView.receiveEvent("startapp", params)
     }
     ,
