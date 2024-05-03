f651e2a355ca80abd77e35c10fc9ec80429ebbb9
irlandets1980 <irlandets1980@gmail.com>
2024-05-03 21:04:39 +0300

    описание коммита11111111110

--- quasar.config.js
+++ quasar.config.js
@@ -55,8 +55,11 @@ module.exports = configure(function (ctx) {
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-build
     build: {
       env: {
-        API_URL: 'https://api.rostgmu.ru/',
+        API_URL: 'http://192.168.0.103:5000/',
       },
+      // env: {
+      // API_URL: 'https://api.rostgmu.ru/',
+      // },
 
       vueRouterMode: 'hash',
 
